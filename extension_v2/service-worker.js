b893409197820cc9f87d84376e90477b9b62970f
RafaelUrquiaga2 <u202010506@upc.edu.pe>
2026-02-07 20:55:27 -0500

    feat(service-worker): logic to register entry times with activated tabs

--- extension_v2/service-worker.js
+++ extension_v2/service-worker.js
@@ -20,86 +20,9 @@ async function handleUpdated(tabId, changeInfo, tabInfo) {
 
     if (!isHttpUrl(tabInfo.url)) return;
 
-    let new_website = { dominio: new URL(tabInfo.url).hostname };
+    let new_hostname = new URL(tabInfo.url).hostname;
 
-    try {
-
-      // REGISTRAMOS EL WEBSITE EN LA BD
-
-      let response1 = await fetch(`http://127.0.0.1:8000/api/v1/websites`, {
-        method: "POST",
-        body: JSON.stringify(new_website),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response1.ok)
-        throw new Error(`Error en websites: ${response1.status}`);
-
-      let data = await response1.json();
-      let id_web = data.id;
-
-      const new_website_user = {
-        id_usuarios: USER_ID,
-        id_sitios_web: id_web,
-        id_categorias_web: DEFAULT_CATEGORY_ID,
-        origen: DEFAULT_ORIGIN
-      };
-
-      // REGISTRAMOS EL WEBSITE POR USUARIO EN LA BD
-
-      let response2 = await fetch(`http://127.0.0.1:8000/api/v1/website-users`, {
-        method: "POST",
-        body: JSON.stringify(new_website_user),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response2.ok)
-        throw new Error(`Error en website-users: ${response2.status}`);
-
-      let data2 = await response2.json();
-      let id_web_user = data2.id;
-
-      let new_web_visited = {
-        id_usuarios: USER_ID,
-        id_sitios_web_usuario: id_web_user,
-        fecha_hora_ingreso: new Date(Date.now())
-      };
-
-      // REGISTRAMOS LA FECHA DE ENTRADA AL WEBSITE EN LA BD
-
-      let response3 = await fetch(`http://127.0.0.1:8000/api/v1/website-visited`, {
-        method: "POST",
-        body: JSON.stringify(new_web_visited),
-        headers: {
-          "Content-Type": "application/json",
-        }
-      });
-
-      if (!response3.ok)
-        throw new Error(`Error en website-user-visited: ${response3.status}`);
-
-      let data3 = await response3.json();
-
-      // GUARDAMOS EL ID DE LA PESTAÑA JUNTO CON SU ID DE WEBSITE_VISITADO EN LA BD
-      chrome.storage.local.set({ [tabId.toString()]: data3.id }); // key -> tabId, value -> data3.id
-
-      /*
-      chrome.storage.local.set({
-        focusguard_webs: {
-          tab_id: tabId,
-          db_id: data3.id
-        }
-      });
-
-       */
-
-    } catch (e) {
-      console.error(e.message);
-    }
+    await complete_register_web(tabId, new_hostname);
 
   }
 
@@ -157,41 +80,124 @@ async function handleActivated(activeInfo) {
   let last_tabId = last_web["last_web_activated"];
   let web_visited = await chrome.storage.local.get(last_tabId.toString());
 
-  // verificamos si el web_visited existe
-  if (web_visited[last_tabId.toString()] === undefined) {
-    chrome.storage.local.set({ last_web_activated: activeInfo.tabId });
-    return;
-  }
-
   let id_web_visited = web_visited[last_tabId.toString()];
   let web_visited_updated = {fecha_hora_salida: new Date(Date.now())};
 
+  // Si el ultimo web_visited existe en el local storage entonces se hace el try-catch
+  // Cuando puede no existir?: Cuando la ultima web no es http
+  if (!(web_visited[last_tabId.toString()] === undefined)) {
+    try {
+      let response = await fetch(`http://127.0.0.1:8000/api/v1/website-visited/${id_web_visited}`, {
+        method: "PATCH",
+        body: JSON.stringify(web_visited_updated),
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      if (!response.ok)
+        throw new Error(`Error en handleActivated() al actualizar la salida en website-user-visited: ${response.status}`);
+
+      //chrome.storage.local.remove(last_tabId.toString());
+
+    } catch (e) {
+      console.error(e.message);
+    }
+  }
+
+  chrome.storage.local.set({ last_web_activated: activeInfo.tabId });
+
+  // __________________________________________
+  // REGISTRO DE INGRESO A PESTAÑA YA EXISTENTE
+  // __________________________________________
+
+  let tab_info = await chrome.tabs.get(activeInfo.tabId);
+
+  if(tab_info.status === 'complete' && tab_info.url) {
+
+    if (!isHttpUrl(tab_info.url)) return;
+
+    let activated_hostname = new URL(tab_info.url).hostname;
+
+    await complete_register_web(activeInfo.tabId, activated_hostname);
+
+  }
+
+}
+
+// Registro completo en las tablas sitios_web, sitios_web_usuario y sitios_web_visitados
+async function complete_register_web(tabId, hostname) {
+
+  let new_web = { dominio: hostname };
+
   try {
-    let response = await fetch(`http://127.0.0.1:8000/api/v1/website-visited/${id_web_visited}`, {
-      method: "PATCH",
-      body: JSON.stringify(web_visited_updated),
+
+    // REGISTRAMOS EL WEBSITE EN LA BD
+
+    let response1 = await fetch(`http://127.0.0.1:8000/api/v1/websites`, {
+      method: "POST",
+      body: JSON.stringify(new_web),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response1.ok)
+      throw new Error(`Error en websites: ${response1.status}`);
+
+    let data = await response1.json();
+    let id_web = data.id;
+
+    const new_website_user = {
+      id_usuarios: USER_ID,
+      id_sitios_web: id_web,
+      id_categorias_web: DEFAULT_CATEGORY_ID,
+      origen: DEFAULT_ORIGIN
+    };
+
+    // REGISTRAMOS EL WEBSITE POR USUARIO EN LA BD
+
+    let response2 = await fetch(`http://127.0.0.1:8000/api/v1/website-users`, {
+      method: "POST",
+      body: JSON.stringify(new_website_user),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response2.ok)
+      throw new Error(`Error en website-users: ${response2.status}`);
+
+    let data2 = await response2.json();
+    let id_web_user = data2.id;
+
+    let new_web_visited = {
+      id_usuarios: USER_ID,
+      id_sitios_web_usuario: id_web_user,
+      fecha_hora_ingreso: new Date(Date.now())
+    };
+
+    // REGISTRAMOS LA FECHA DE ENTRADA AL WEBSITE EN LA BD
+
+    let response3 = await fetch(`http://127.0.0.1:8000/api/v1/website-visited`, {
+      method: "POST",
+      body: JSON.stringify(new_web_visited),
       headers: {
         "Content-Type": "application/json",
       }
     });
 
-    if (!response.ok)
-      throw new Error(`Error en handleActivated() al actualizar la salida en website-user-visited: ${response.status}`);
+    if (!response3.ok)
+      throw new Error(`Error en website-user-visited: ${response3.status}`);
 
-    //chrome.storage.local.remove(last_tabId.toString());
+    let data3 = await response3.json();
+
+    // GUARDAMOS EL ID DE LA PESTAÑA JUNTO CON SU ID DE WEBSITE_VISITADO EN LA BD
+    chrome.storage.local.set({ [tabId.toString()]: data3.id }); // key -> tabId, value -> data3.id
 
   } catch (e) {
     console.error(e.message);
   }
-
-  chrome.storage.local.set({ last_web_activated: activeInfo.tabId });
-
-  // _______________________________________________________
-  // SE REGISTRA LA NUEVA PESTAÑA A DÓNDE SE ESTÁ INGRESANDO
-  // _______________________________________________________
-
-
-
 }
 
 chrome.tabs.onUpdated.addListener(handleUpdated);
